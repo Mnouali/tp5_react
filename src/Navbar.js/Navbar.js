4bd17cf2c6eee8d0f0193cacd8f5352d716efcd2
nouali <mohamedngmail.com>
2023-11-13 00:21:49 +0100

    first commit

--- src/Navbar.js/Navbar.js
+++ src/Navbar.js/Navbar.js
@@ -7,10 +7,10 @@ export default function Navbar() {
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup" style={{display:"flex",justifyContent:"center"}}>
     <div className="navbar-nav" style={{display:"flex",justifyContent:"space-around"}}>
-       <Link to={"/tp6_react"} className="nav-item nav-link active"><li href="#">Clacul</li></Link>
-      <Link to={"tp6_react/tp2"} className="nav-item nav-link active"><li href="#">Slider</li></Link>
-       <Link to={"tp6_react/tp3"} className="nav-item nav-link active" ><li href="#">To do list</li></Link>
-       <Link to={"tp6_react/tp4"}className="nav-item nav-link active"><li  href="#">Pagination</li></Link>    
+       <Link to={"/tp5_react"} className="nav-item nav-link active"><li href="#">Clacul</li></Link>
+      <Link to={"tp5_react/tp2"} className="nav-item nav-link active"><li href="#">Slider</li></Link>
+       <Link to={"tp5_react/tp3"} className="nav-item nav-link active" ><li href="#">To do list</li></Link>
+       <Link to={"tp5_react/tp4"}className="nav-item nav-link active"><li  href="#">Pagination</li></Link>    
     </div>
   </div>
 </nav>

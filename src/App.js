9305c3b8461df8110b68afa461525fffc71bbff8
nouali <mohamedngmail.com>
2023-11-13 00:16:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path='/tp6_react' element={<Calcul/>}></Route>
-          <Route path='tp2' element={<Slider/>}></Route>
-          <Route path='tp3'  element={<Form/>}></Route>
-          <Route path='tp4'  element={<Pagination/>}></Route>
+          <Route path='tp6_react/tp2' element={<Slider/>}></Route>
+          <Route path='tptp6_react/tp3'  element={<Form/>}></Route>
+          <Route path='tp6_react/tp4' element={<Pagination/>}></Route>
           <Route path='*'  element={<h1>Not Found 404</h1>}></Route>
 
         </Routes>
--- src/Navbar.js/Navbar.js
+++ src/Navbar.js/Navbar.js
@@ -8,9 +8,9 @@ export default function Navbar() {
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup" style={{display:"flex",justifyContent:"center"}}>
     <div className="navbar-nav" style={{display:"flex",justifyContent:"space-around"}}>
        <Link to={"/tp6_react"} className="nav-item nav-link active"><li href="#">Clacul</li></Link>
-      <Link to={"tp2"} className="nav-item nav-link active"><li href="#">Slider</li></Link>
-       <Link to={"tp3"} className="nav-item nav-link active" ><li href="#">To do list</li></Link>
-       <Link to={"tp4"}className="nav-item nav-link active"><li  href="#">Pagination</li></Link>    
+      <Link to={"tp6_react/tp2"} className="nav-item nav-link active"><li href="#">Slider</li></Link>
+       <Link to={"tp6_react/tp3"} className="nav-item nav-link active" ><li href="#">To do list</li></Link>
+       <Link to={"tp6_react/tp4"}className="nav-item nav-link active"><li  href="#">Pagination</li></Link>    
     </div>
   </div>
 </nav>
